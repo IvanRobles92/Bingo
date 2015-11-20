ba37915dbb5c9cd44d2b6b1c0c7e29f63fd09e5d
Ivan Robles <robleshuelma@gmail.com>
2015-11-20 12:44:14 +0100

    genera numeros aleatorios

--- Bingo.php
+++ Bingo.php
@@ -4,6 +4,7 @@
 </head>
 <body>
 <?php
+session_start();
 /**
  * Created by PhpStorm.
  * User: alumno
@@ -31,11 +32,41 @@ for ($i = 0; $i <= 10; $i++) {
     echo "</tr>";
 }
 echo "</table>";
+
+function galeatorios(){
+    //$numeros;=array();
+    $numeros=rand(1,90);
+    //for($i=1;$i<$n;$i++){
+    // $numeros[$i]=rand(1,49);
+    /*for($j=0;$j<=$i-1;$j++){
+        if($numeros[$j]==$numeros[$i]){
+            $i--;
+            break;
+        }
+    }*/
+    //}
+    return $numeros;
+
+}
+
 ?>
     <form action="Bingo.php" method="post">
-        <input type="button" id="genera" value="Obtener numero"/>
-        <input type="button" id="nuevo" value="NuevoJuego"/>
+        <input type="submit" name="genera" value="Obtener numero"/>
+        <input type="submit" name="nuevo" value="NuevoJuego"/>
     </form>
 </body>
 </html>
+<?php
+if(isset($_POST["genera"]))
+{
+    $_SESSION["numero"]=galeatorios();
+    echo $_SESSION["numero"];
+}
+
+if(isset($_POST["nuevo"]))
+{
+session_destroy();
+}
+
+?>
 
